84a8a763f1133aef98594a3b718abe1933966cba
Roman Oshchypok <r.oshchypok@gmail.com>
2017-03-05 10:40:42 +0200

    logo

--- js/regal_script.js
+++ js/regal_script.js
@@ -535,7 +535,47 @@ $(window).load(function(){
 	  var filterValue = $(this).attr('data-filter');
 	  $grid.isotope({ filter: filterValue });
 	});
-	
-	
+
+	/*
+	 * Translated default messages for the jQuery validation plugin.
+	 * Locale: UK (Ukrainian; українська мова)
+	 */
+    $.extend( $.validator.messages, {
+        required: "Це поле необхідно заповнити.",
+        remote: "Будь ласка, введіть правильне значення.",
+        email: "Будь ласка, введіть коректну адресу електронної пошти.",
+        url: "Будь ласка, введіть коректний URL.",
+        date: "Будь ласка, введіть коректну дату.",
+        dateISO: "Будь ласка, введіть коректну дату у форматі ISO.",
+        number: "Будь ласка, введіть число.",
+        digits: "Вводите потрібно лише цифри.",
+        creditcard: "Будь ласка, введіть правильний номер кредитної карти.",
+        equalTo: "Будь ласка, введіть таке ж значення ще раз.",
+        extension: "Будь ласка, виберіть файл з правильним розширенням.",
+        maxlength: $.validator.format( "Будь ласка, введіть не більше {0} символів." ),
+        minlength: $.validator.format( "Будь ласка, введіть не менше {0} символів." ),
+        rangelength: $.validator.format( "Будь ласка, введіть значення довжиною від {0} до {1} символів." ),
+        range: $.validator.format( "Будь ласка, введіть число від {0} до {1}." ),
+        max: $.validator.format( "Будь ласка, введіть число, менше або рівно {0}." ),
+        min: $.validator.format( "Будь ласка, введіть число, більше або рівно {0}." )
+    } );
+
+    $.datepicker.regional['uk'] = {
+        closeText: 'Закрити',
+        prevText: '&#x3C;',
+        nextText: '&#x3E;',
+        currentText: 'Сьогодні',
+        monthNames: ['Січень','Лютий','Березень','Квітень','Травень','Червень','Липень','Серпень','Вересень','Жовтень','Листопад','Грудень'],
+        monthNamesShort: ['Січ','Лют','Бер','Кві','Тра','Чер','Лип','Сер','Вер','Жов','Лис','Гру'],
+        dayNames: ['неділя','понеділок','вівторок','середа','четвер','п’ятниця','субота'],
+        dayNamesShort: ['нед','пнд','вів','срд','чтв','птн','сбт'],
+        dayNamesMin: ['Нд','Пн','Вт','Ср','Чт','Пт','Сб'],
+        weekHeader: 'Тиж',
+        dateFormat: 'dd/mm/yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''};
+    $.datepicker.setDefaults($.datepicker.regional['uk']);
 });
 
